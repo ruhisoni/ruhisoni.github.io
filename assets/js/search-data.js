2dc7b7a3d839f0f7e9b3fde9f8713ef1f6802851
ruhisoni <29504241+ruhisoni@users.noreply.github.com>
2025-05-26 10:44:51 +0000

    Deploying to gh-pages from @ ruhisoni/ruhisoni.github.io@135fd6f3e54fb9f9ce2bb93b0c34f6c9ee3a438c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -424,6 +424,11 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
+            },},{id: "projects-project-4",
+          title: 'project 4',
+          description: "another without an image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
             },},{
         id: 'social-email',
         title: 'email',
